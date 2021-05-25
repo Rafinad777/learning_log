e77a77f918cc41bf45529b487c30c2149f92d180
roman <roman@MacBook-Pro-RomanMac.local>
2021-05-25 14:35:26 +0300

    Added custom 404 and 500 erro pages.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -57,10 +57,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'learning_log.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
