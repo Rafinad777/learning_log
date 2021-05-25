2a87841c9fc176a46cf2d8fe96672d3880a35da4
roman <roman@MacBook-Pro-RomanMac.local>
2021-05-25 14:42:50 +0300

    Get_object_or_404.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from .models import Topic, Entry
@@ -26,10 +26,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
 	"""Выводит одну тему и все ее записи."""
-	topic = Topic.objects.get(id = topic_id)
+	topic = get_object_or_404(Topic, id = topic_id)
 	# Проверка того, что тема принадлежит текущему пользователю
-	# if topic.owner != request.user:
-	# 	raise Http404
 	check_topic_owner(request, topic)
 	entries = topic.entry_set.order_by('-date_added') # -date_added
 	context = {'topic': topic, 'entries': entries}
