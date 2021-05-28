90b41054140f80d23c8f366f7f22b869566cc363
roman <roman@MacBook-Pro-RomanMac.local>
2021-05-28 16:51:36 +0300

    Push github

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-97tp$r-w&&2h@7y#&1!tt5fb37t5vd9+jj!49zgn9p29n4!ze4'
+# NO SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
